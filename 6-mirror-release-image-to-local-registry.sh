6f7e2197acd8616146de73d1888906a91d8aee0d
Josef Meier <jomeier@users.noreply.github.com>
2021-02-21 15:45:47 +0100

    Update 6-mirror-release-image-to-local-registry.sh
    Change final mirror.

--- 6-mirror-release-image-to-local-registry.sh
+++ 6-mirror-release-image-to-local-registry.sh
@@ -1,7 +1,5 @@
-REPO_MIRROR_LOCATION=quay.io/<yourname>/okd-mirror
-
 oc adm -a ${PULL_SECRET} release mirror \
-  --from=${REPO_OKD_RELEASE} \
-  --to=${REPO_MIRROR} \
-  --to-release-image=${REPO_MIRROR_LOCATION}:${TAG}
+  --from=${REPO_MIRROR} \
+  --to=${REPO_FINAL_MIRROR} \
+  --to-release-image=${REPO_FINAL_MIRROR}:${TAG}
 
